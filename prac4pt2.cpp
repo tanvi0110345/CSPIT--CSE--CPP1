66b48e46254bda7335506ce9860124cae12540ef
tanvi0110345 <Pateltisha01102008@gmail.com>
2026-08-30 21:59:14 +0530

    Add files via upload

---
+++ prac4pt2.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+ int enrollment;
+ char name[20];
+ char branch[20];
+ int semester;
+ char gender[05];
+ char bloodgroup[05];
+ char dob[20];
+
+ int n;
+ // some calculation
+
+ float mathamatics, physics, cpf;
+ float total, average, percentage,result;
+
+ string grade, remark;
+
+ cout<< "*************************************************************************************\n";
+ cout<< "                              STUDENT  RECORD MANAGMENT SYSTEM\n";
+ cout<< "*************************************************************************************\n";
+
+ cout<< " Enter enrollment: ";
+ cin>> enrollment;
+
+ cout<< "Enter name: ";
+ cin>> name;
+
+ cout<< "Enter branch: ";
+ cin>> branch;
+
+ cout<< "enter semester: ";
+ cin>> semester;
+
+ cout<< "Enter date of birth: ";
+ cin>> dob;
+
+ cout<< " Enter gender: ";
+ cin>> gender;
+
+ cout<< "Enter bloodgroup: ";
+ cin>> bloodgroup;
+
+// for calculation and students input information
+ cout<< "************************************************************************************\n";
+ cout << "                     STUDENT INFORMATION\n";
+ cout<< "************************************************************************************\n";
+
+ //cout<< " mathamatics: ";
+ //cin>> mathamatics;
+ mathamatics = 80;
+
+ cout<< " physics: ";
+ cin>> physics;
+
+ //cout<< "cpf: ";
+// cin>> cpf;
+cpf = 70;
+
+ // calculation
+
+ total = mathamatics + physics + cpf;
+ average = total / 3;
+ percentage = (total/300) * 100;
+
+ cout<< "************************************************************************************\n";
+ cout<<"                       STUDENT ACADAMIC REPORT\n";
+ cout<< "************************************************************************************\n";
+ 
+ cout<< left << setw(30) << "enrollment number"
+     << ": " << enrollment << endl;
+
+cout<< left << setw(30) << "name"
+    << ": " << name << endl;
+
+cout<< left << setw(30) << "branch"
+    << ": " << branch   << endl;
+
+cout<< left << setw(30) << "semester"
+    << ": " << semester << endl;
+
+cout<< left << setw(30) << "gender"
+    << ": " << gender << endl;
+    
+cout<< left << setw(30) << "bloodgroup"
+    << ": " << bloodgroup << endl;
+
+ cout<< left << setw(30) << "date of birth"
+    << ": " << dob << endl;
+
+    // now let's do increment decrement...!
+
+cout<< "***************************************************************************************\n";
+cout<< "                         CALCULATION\n";
+cout<< "***************************************************************************************\n";
+
+cout<< left << setw(30) << "total"
+    << ": " << total << endl;
+
+cout<< left << setw(30) << "average"
+    << ": " << average << endl;
+
+cout << left << setw(30) << "percentage"
+     << ": " << percentage << endl;
+
+
+cout<< "****************************************************************************************\n";
+cout<< "                          INCREMENT DECREMENT REPORT\n";
+cout<< "****************************************************************************************\n";
+
+cout<< left << setw(30) << "cpf"
+    << ": " << cpf << endl;
+    ++cpf;
+
+cout<< left << setw(30) << "cpf marks after increament"
+    << ": " << cpf << endl;
+    --cpf;
+
+cout<< left << setw(30) << "cpf marks after dicreament"
+    << ": " << cpf << endl;
+cpf++;
+
+cout<< left << setw(30) << "cpf marks after postfixed increament operator"
+    << ": " << cpf << endl;
+    cpf--;
+
+cout<< left << setw(30) << "cpf marks after postfixed dicreament operator"
+    << ": " << cpf << endl;
+    cpf;
+
+    cout <<"------------------------------------------------------------------------------------\n";
+    cout<<"                                    RESULT\n";
+    cout <<"------------------------------------------------------------------------------------\n";
+
+    result = ++cpf + cpf++ + --mathamatics + ++mathamatics - mathamatics--;
+
+    cout<< left << setw(30) << "result"
+        << ": " << result << endl;
+
+// student result if he/she passed or not let check it.
+// conditional statments.
+
+cout<< "----------------------------------------------------------------------------------------\n";
+cout<< "                                STUDENT RESULT\n";
+cout<< "----------------------------------------------------------------------------------------\n";
+
+if( percentage >= 40)
+{
+    cout<< "Result: PASSED"<< ": "<<percentage<< endl;
+    cout<< "CONGRATULATIONS!!! You have successfully passed.";
+
+}
+else if( percentage < 40)
+{
+    cout<< "Result: FAILED"<< ": "<<percentage<< endl;
+    cout << "Better luck next time.";
+}
+
+cout<<"\n----------------------------------------------------------------------------------------\n";
+cout<< "                             GRADES AND REMARKS\n";
+cout<<"----------------------------------------------------------------------------------------\n";
+
+
+// ELSE IF LEADER.
+
+if( percentage > 90)
+{
+  grade = "O";
+  remark = "outstanding";
+}
+
+else if( percentage > 80)
+{
+    grade = "A+";
+    remark = "Exellent";
+}
+else if( percentage > 70)
+{
+    grade = "A";
+    remark = "Very Good";
+}
+else if( percentage > 60)
+{
+    grade = "B+";
+    remark = "Good";
+}
+else if( percentage > 50)
+{
+    grade = "B";
+    remark = "Need to improvment";
+}
+else if( percentage > 40)
+{
+    grade = "C";
+    remark = "Not good";
+}
+else 
+{
+    grade = "F";
+    remark = "Failed";
+}
+
+cout<< "GRADE: "<< grade << endl;
+cout<< "REMARK: "<< remark << endl;
+
+
+return 0;
+}
